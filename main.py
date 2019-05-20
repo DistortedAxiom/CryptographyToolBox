a69d32326720dbd95c1cdabb4a1d07955546f926
DistortedAxiom <48456332+DistortedAxiom@users.noreply.github.com>
2019-05-20 18:15:59 -0400

    First

---
+++ main.py
@@ -0,0 +1,10 @@
+from strings import StringReplacer
+
+def start():
+    x = input("Enter a string")
+    modified = StringReplacer(x)
+    modified.replacer()
+
+
+if __name__ == '__main__':
+    start()
---
+++ strings.py
@@ -0,0 +1,7 @@
+class StringReplacer:
+    def __init__(self, string):
+        self.string = string
+
+    def replacer(self):
+        string = "this was replaced"
+        print(string)
