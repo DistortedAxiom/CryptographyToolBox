9a6e05b756f82f71cd54f1a4c04994a610985313
DistortedAxiom <48456332+DistortedAxiom@users.noreply.github.com>
2019-05-24 20:18:31 -0400

    Changed MainWindow to QStackedWidget and implemented index changing

--- eSolverToolbox/main.py
+++ eSolverToolbox/main.py
@@ -1,25 +1,98 @@
-from PyQt5.QtWidgets import *
-from PyQt5 import QtWidgets
-
 import sys
+from PyQt5 import QtCore, QtWidgets
 
-from windows.MainWindow import Ui_MainWindow
+
+class MainWindow(QtWidgets.QWidget):
+
+    switch_window = QtCore.pyqtSignal(str)
+
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
+        self.setWindowTitle('Main Window')
+
+        layout = QtWidgets.QGridLayout()
+
+        self.line_edit = QtWidgets.QLineEdit()
+        layout.addWidget(self.line_edit)
+
+        self.button = QtWidgets.QPushButton('Switch Window')
+        self.button.clicked.connect(self.switch)
+        layout.addWidget(self.button)
+
+        self.setLayout(layout)
+
+    def switch(self):
+        self.switch_window.emit(self.line_edit.text())
 
 
-class MainWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
-        self.setupUi(self)
+class WindowTwo(QtWidgets.QWidget):
 
-        self.stringButton.pressed.connect(self.stringMenu)
+    def __init__(self, text):
+        QtWidgets.QWidget.__init__(self)
+        self.setWindowTitle('Window Two')
 
-        self.show()
+        layout = QtWidgets.QGridLayout()
 
-    def stringMenu(self):
-        print("String button testing")
+        self.label = QtWidgets.QLabel(text)
+        layout.addWidget(self.label)
+
+        self.button = QtWidgets.QPushButton('Close')
+        self.button.clicked.connect(self.close)
+
+        layout.addWidget(self.button)
+
+        self.setLayout(layout)
+
+
+class Login(QtWidgets.QWidget):
+
+    switch_window = QtCore.pyqtSignal()
+
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
+        self.setWindowTitle('Login')
+
+        layout = QtWidgets.QGridLayout()
+
+        self.button = QtWidgets.QPushButton('Login')
+        self.button.clicked.connect(self.login)
+
+        layout.addWidget(self.button)
+
+        self.setLayout(layout)
+
+    def login(self):
+        self.switch_window.emit()
+
+
+class Controller:
+
+    def __init__(self):
+        pass
+
+    def show_login(self):
+        self.login = Login()
+        self.login.switch_window.connect(self.show_main)
+        self.login.show()
+
+    def show_main(self):
+        self.window = MainWindow()
+        self.window.switch_window.connect(self.show_window_two)
+        self.login.close()
+        self.window.show()
+
+    def show_window_two(self, text):
+        self.window_two = WindowTwo(text)
+        self.window.close()
+        self.window_two.show()
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    controller = Controller()
+    controller.show_login()
+    sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication([])
-    window = MainWindow()
-    app.exec_()
+    main()
--- eSolverToolbox/windows/MainWindow.py
+++ eSolverToolbox/windows/MainWindow.py
@@ -8,60 +8,62 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(775, 563)
-        MainWindow.setStyleSheet("background-color: rgb(206, 206, 206);")
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.centralwidget)
-        self.verticalLayout_3.setObjectName("verticalLayout_3")
-        self.appTitle = QtWidgets.QLabel(self.centralwidget)
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(967, 685)
+        self.horizontalLayout = QtWidgets.QHBoxLayout(Form)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.stackedWidget = QtWidgets.QStackedWidget(Form)
+        self.stackedWidget.setStyleSheet("background-color: rgb(214, 107, 0);")
+        self.stackedWidget.setObjectName("stackedWidget")
+        self.page_1 = QtWidgets.QWidget()
+        self.page_1.setObjectName("page_1")
+        self.gridLayout = QtWidgets.QGridLayout(self.page_1)
+        self.gridLayout.setObjectName("gridLayout")
+        self.stringButton = QtWidgets.QPushButton(self.page_1)
+        self.stringButton.setObjectName("stringButton")
+        self.gridLayout.addWidget(self.stringButton, 2, 1, 1, 1)
+        self.appTitle = QtWidgets.QLabel(self.page_1)
         font = QtGui.QFont()
         font.setPointSize(18)
         font.setBold(True)
         font.setWeight(75)
         self.appTitle.setFont(font)
         self.appTitle.setObjectName("appTitle")
-        self.verticalLayout_3.addWidget(self.appTitle, 0, QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
-        self.horizontalLayout_1 = QtWidgets.QHBoxLayout()
-        self.horizontalLayout_1.setContentsMargins(-1, 0, -1, -1)
-        self.horizontalLayout_1.setSpacing(50)
-        self.horizontalLayout_1.setObjectName("horizontalLayout_1")
-        self.stringButton = QtWidgets.QPushButton(self.centralwidget)
-        self.stringButton.setObjectName("stringButton")
-        self.horizontalLayout_1.addWidget(self.stringButton)
-        self.feature_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.gridLayout.addWidget(self.appTitle, 2, 0, 1, 1)
+        self.feature_2 = QtWidgets.QPushButton(self.page_1)
         self.feature_2.setObjectName("feature_2")
-        self.horizontalLayout_1.addWidget(self.feature_2)
-        self.feature_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.feature_3.setObjectName("feature_3")
-        self.horizontalLayout_1.addWidget(self.feature_3)
-        self.verticalLayout_3.addLayout(self.horizontalLayout_1)
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 775, 26))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
+        self.gridLayout.addWidget(self.feature_2, 3, 1, 1, 1)
+        self.stackedWidget.addWidget(self.page_1)
+        self.page_2 = QtWidgets.QWidget()
+        self.page_2.setObjectName("page_2")
+        self.label_2 = QtWidgets.QLabel(self.page_2)
+        self.label_2.setGeometry(QtCore.QRect(360, 90, 211, 61))
+        self.label_2.setObjectName("label_2")
+        self.stackedWidget.addWidget(self.page_2)
+        self.horizontalLayout.addWidget(self.stackedWidget)
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.retranslateUi(Form)
+        self.stackedWidget.setCurrentIndex(0)
+        QtCore.QMetaObject.connectSlotsByName(Form)
 
-
-    def retranslateUi(self, MainWindow):
+    def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.appTitle.setText(_translate("MainWindow", "Welcome to ESolver"))
-        self.stringButton.setText(_translate("MainWindow", "Strings"))
-        self.feature_2.setText(_translate("MainWindow", "Feature 2"))
-        self.feature_3.setText(_translate("MainWindow", "Feature 3"))
-
-    def printMessage(self):
-        print("Hello")
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.stackedWidget.setToolTip(_translate("Form", "<html><head/><body><p><br/></p></body></html>"))
+        self.stringButton.setText(_translate("Form", "Strings"))
+        self.appTitle.setText(_translate("Form", "Welcome to ESolver"))
+        self.feature_2.setText(_translate("Form", "Feature 2"))
+        self.label_2.setText(_translate("Form", "Second Page"))
 
 
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
 
