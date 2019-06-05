028536bb2004fe71d796bf664a72e9345a7a2916
DistortedAxiom <48456332+DistortedAxiom@users.noreply.github.com>
2019-06-05 11:32:49 -0400

    testing some replacement methods

--- eSolverToolbox/methods/string_replacer_dialog.py
+++ eSolverToolbox/methods/string_replacer_dialog.py
@@ -10,6 +10,9 @@ class StringReplacerWindow(QDialog, Ui_Dialog):
     getter_string = ""
     getter_list = []
     replaced_string = ""
+    letter_dict = {}
+    key_value_pair = {}
+
 
     def __init__(self, *args, **kwargs):
         super(StringReplacerWindow, self).__init__(*args, **kwargs)
@@ -19,6 +22,7 @@ class StringReplacerWindow(QDialog, Ui_Dialog):
         self.show()
 
 
+
     @staticmethod
     def string_replace_init(value):
         StringReplacerWindow.getter_string = value
@@ -26,10 +30,25 @@ class StringReplacerWindow(QDialog, Ui_Dialog):
         StringReplacerWindow.getter_list = list(StringReplacerWindow.getter_string)
 
     def char_change(self):
-        print(StringReplacerWindow.getter_list[0:5])
+        print(StringReplacerWindow.getter_list[0:10])
 
     def input_getter(self):
-        A = self.letterA.text()
+        pass
+
+
+#We can iterate from list, IE: list_item: self.letter*(wild_card).text()
+    def recursion_tester(self):
+        pass
+
+    def key_value_pair_init(self):
+        StringReplacerWindow.key_value_pair = {
+            "A": self.letterA.text(),
+            "B": self.letterB.text(),
+            "C": self.letterC.text(),
+            "D": self.lettetD.text(),
+        }
+
+
 
 
 
